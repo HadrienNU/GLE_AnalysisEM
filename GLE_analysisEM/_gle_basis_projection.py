5d734057f2837cbb8ef71e3966eb5a041cb4697b
Hadrien <hadrien@langevin>
2020-12-06 00:01:08 +0100

    End of the day

--- GLE_analysisEM/_gle_basis_projection.py
+++ GLE_analysisEM/_gle_basis_projection.py
@@ -9,6 +9,83 @@ from sklearn.utils.validation import check_is_fitted
 from sklearn.utils import check_array
 
 
+class GLE_BasisTransform(TransformerMixin, BaseEstimator):
+    """ A simple transformer that give values of the linear basis along the trajectories.
+
+    Parameters
+    ----------
+    dim_x : int, default=1
+        The number of visible dimensions.
+
+    basis_type : dict default={"name": "linear"}
+        Give the wanted basis projection
+        Must be one of::
+            {"name": "linear"} : coefficients are initialized using markovian approximation.
+            {"name": "polynomial", "poly_deg": poly_deg} : coefficients are initialized at values provided by the user
+            'random' : coefficients are initialized randomly.
+
+    Attributes
+    ----------
+    basis_type_  : str
+        The type of the basis.
+    """
+
+    def __init__(self, dim_x=1, basis_type={"name": "linear"}, model="ABOBA"):
+        self.dim_x = dim_x
+        self.basis_type = basis_type
+        self.model = model
+
+    def fit(self, X, y=None):
+        """A reference implementation of a fitting function for a transformer.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, dim_x)
+            The training input samples.
+        y : None
+            There is no need of a target in a transformer, yet the pipeline API
+            requires this parameter.
+
+        Returns
+        -------
+        self : object
+            Returns self.
+        """
+        self.fitted_ = True
+        return self
+
+    def _check_basis_type(self):
+        """Check the inputed parameter for the basis type
+        """
+
+    def transform(self, X):
+        """ A reference implementation of a transform function.
+        Take the x_{1/2} as input and output basis expansion
+
+        Parameters
+        ----------
+        X : {array-like, sparse-matrix}, shape (n_samples, n_features)
+            The input samples.
+
+        Returns
+        -------
+        X_transformed : array, shape (n_samples, n_features)
+
+        """
+        # Check is fit had been called
+        check_is_fitted(self, "fitted_")
+
+        # Input validation
+        X = check_array(X, ensure_min_samples=4, allow_nd=True)
+        dt = X[1, 0] - X[0, 0]
+        xhalf = X[:, 1 : 1 + self.dim_x] + 0.5 * dt * X[:, 1 + self.dim_x : 1 + 2 * self.dim_x]
+        if self.basis_type["name"] == "linear":
+            bk = xhalf
+        elif self.basis_type["name"] == "polynomial":
+            bk = xhalf
+        return np.hstack((X, bk))
+
+
 class GLE_LinearBasis(TransformerMixin, BaseEstimator):
     """ A simple transformer that give values of the linear basis along the trajectories.
 
@@ -134,7 +211,7 @@ class GLE_PolynomialBasis(TransformerMixin, BaseEstimator):
 class GLE_BSplinesBasis(TransformerMixin, BaseEstimator):
     """ A transformer that give values of for a B-splines basis along the trajectories.
     .. todo:: To implement the Bsplines Features
-    
+
     Parameters
     ----------
     dim_x : int, default=1
--- examples/interface_scikit-learn.py
+++ examples/interface_scikit-learn.py
@@ -8,9 +8,10 @@ An example plot of :class:`GLE_analysisEM.GLE_Estimator`
 import pandas as pd
 
 # from matplotlib import pyplot as plt
-from GLE_analysisEM import GLE_Estimator
+from GLE_analysisEM import GLE_Estimator, GLE_LinearBasis
 from GLE_analysisEM.utils import loadTestDatas_est
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
 
 paths = ["../GLE_analysisEM/tests/0_trajectories.dat", "../GLE_analysisEM/tests/1_trajectories.dat", "../GLE_analysisEM/tests/2_trajectories.dat"]
 paths_train, paths_test = train_test_split(paths)
@@ -18,13 +19,16 @@ paths_train, paths_test = train_test_split(paths)
 X_train, idx_train, Xh_train = loadTestDatas_est(paths_train, 1, 1)
 X_test, idx_test, Xh_test = loadTestDatas_est(paths_test, 1, 1)
 
+pipe = Pipeline([("basis", GLE_LinearBasis(dim_x=1)), ("em", GLE_Estimator(verbose=1, EnforceFDT=False))])
 # TODO: Make a pipeline
+basis = GLE_LinearBasis(dim_x=1)
+X_basis = basis.fit_transform(X_train)
 
 print(X_train.shape, X_test.shape)
 estimator = GLE_Estimator(verbose=1, EnforceFDT=False)
 # putting together a parameter grid to search over using grid search
-params = {"dim_h": [1, 2], "EnforceFDT": [True, False]}  # setting up the grid search
-gs = GridSearchCV(estimator, params, n_jobs=-1, cv=2)
+params = {"em__dim_h": [1, 2], "em__EnforceFDT": [True, False]}  # setting up the grid search
+gs = GridSearchCV(estimator, params, n_jobs=4, cv=2)
 gs.fit(X_train, idx_trajs=idx_train)
 print("Best parameters set found on development set:")
 print(gs.best_params_)
