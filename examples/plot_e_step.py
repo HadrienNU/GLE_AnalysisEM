9140d648522eb31fdfb63775f38eb4a0ee8ec91c
Hadrien <hadrien.vroylandt@sorbonne-universite.fr>
2021-02-16 17:09:24 +0100

    E step exploration

--- examples/plot_e_step.py
+++ examples/plot_e_step.py
@@ -14,7 +14,7 @@ from GLE_analysisEM import GLE_Estimator, GLE_BasisTransform, sufficient_stats, 
 pd.set_option("display.max_rows", None)
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", None)
-pd.set_option("display.max_colwidth", -1)
+pd.set_option("display.max_colwidth", None)
 
 dim_x = 1
 dim_h = 1
@@ -22,11 +22,11 @@ model = "euler"
 shift = 0
 random_state = None
 force = -np.identity(dim_x)
-A = [[5, 1.0], [-2.0, 0.07]]
-C = 1e-2 * np.identity(dim_x + dim_h)
+A = [[5, 1.0], [-1.0, 2.07]]
+C = np.identity(dim_x + dim_h)  #
 basis = GLE_BasisTransform(basis_type="linear")
 generator = GLE_Estimator(verbose=1, dim_x=dim_x, dim_h=dim_h, EnforceFDT=False, C_init=C, force_init=force, init_params="random", model=model, random_state=random_state)
-X, idx, Xh = generator.sample(n_samples=10000, n_trajs=10, x0=0.0, v0=0.0, basis=basis)
+X, idx, Xh = generator.sample(n_samples=10000, n_trajs=100, x0=0.0, v0=0.0, basis=basis)
 traj_list_h = np.split(Xh, idx)
 time = np.split(X, idx)[0][:, 0]
 for n, traj in enumerate(traj_list_h):
@@ -75,14 +75,40 @@ for n, traj in enumerate(traj_list):
     noise_corr += n_c / len(traj_list)
 print("Estimated datas")
 print(new_stat)
+print("Diff")
+print((new_stat - datas) / np.abs(datas))
 
+
+Pf = np.zeros((dim_x + dim_h, dim_x))
+Pf[:dim_x, :dim_x] = 5e-3 * np.identity(dim_x)
+YX = new_stat["xdx"].T - np.matmul(Pf, np.matmul(force, new_stat["bkx"]))
+XX = new_stat["xx"]
+A = -np.matmul(YX, np.linalg.inv(XX))
+
+
+Pf = np.zeros((dim_x + dim_h, dim_x))
+Pf[:dim_x, :dim_x] = 5e-3 * np.identity(dim_x)
+
+# A = generator.friction_coeffs
+print(A)
+bkbk = np.matmul(Pf, np.matmul(np.matmul(force, np.matmul(new_stat["bkbk"], force.T)), Pf.T))
+bkdx = np.matmul(Pf, np.matmul(force, new_stat["bkdx"]))
+bkx = np.matmul(Pf, np.matmul(force, new_stat["bkx"]))
+
+residuals = new_stat["dxdx"] + np.matmul(A, new_stat["xdx"]) + np.matmul(A, new_stat["xdx"]).T - bkdx.T - bkdx
+residuals += np.matmul(A, np.matmul(new_stat["xx"], A.T)) - np.matmul(A, bkx.T) - np.matmul(A, bkx.T).T + bkbk
+print(residuals, generator.diffusion_coeffs)
+# SST = 0.5 * (residuals + residuals.T)
+
+fig, axs = plt.subplots(1, 1)
 # np.savetxt("E_step.dat", np.vstack((X[:-1, 0], muh[:-1, 0], np.sqrt(Sigh[:-1, 0, 0]), Xh[:-1, 0])).T)
-plt.plot(noise_corr, label="Noise correlation")
-plt.show()
+axs.plot(noise_corr, label="Noise correlation")
+fig, axs = plt.subplots(1, dim_h)
+# plt.show()
 for k in range(dim_h):
-    plt.plot(time[:-1], muh[:, k], label="Prediction (with \\pm 2 \\sigma error lines)", color="blue")
-    plt.plot(time[:-1], muh[:, k] + 2 * np.sqrt(Sigh[:, k, k]), "--", color="blue", linewidth=0.1)
-    plt.plot(time[:-1], muh[:, k] - 2 * np.sqrt(Sigh[:, k, k]), "--", color="blue", linewidth=0.1)
-    plt.plot(time[:-1], traj_list_h[n][:, k], label="Real", color="orange")
-    plt.legend(loc="upper right")
-    plt.show()
+    axs.plot(time[:-1], muh[:, k], label="Prediction (with \\pm 2 \\sigma error lines)", color="blue")
+    axs.plot(time[:-1], muh[:, k] + 2 * np.sqrt(Sigh[:, k, k]), "--", color="blue", linewidth=0.1)
+    axs.plot(time[:-1], muh[:, k] - 2 * np.sqrt(Sigh[:, k, k]), "--", color="blue", linewidth=0.1)
+    axs.plot(time[:-1], traj_list_h[n][:, k], label="Real", color="orange")
+    axs.legend(loc="upper right")
+plt.show()
