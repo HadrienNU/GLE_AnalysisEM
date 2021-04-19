144b56edee63874cb21b6b87aa76456a297e07ae
Hadrien <hadrien@langevin>
2021-04-19 20:15:05 +0200

    Add initial likelihood

--- examples/plot_likelihood_evolution.py
+++ examples/plot_likelihood_evolution.py
@@ -24,23 +24,23 @@ model = "euler_fv"
 force = -np.identity(dim_x)
 max_iter = 10
 N_big_steps = 150
-ntrajs = 5
+ntrajs = 25
 
 pot_gen = GLE_BasisTransform(basis_type="linear")
 
 # Trajectory generation
 generator = GLE_Estimator(verbose=2, dim_x=dim_x, dim_h=dim_h, EnforceFDT=True, force_init=force, init_params="random", model=model, basis=pot_gen, random_state=random_state)
-X, idx, Xh = generator.sample(n_samples=5000, n_trajs=ntrajs, x0=0.0, v0=0.0)
-print("Real parameters", generator.get_coefficients())
-
+X, idx, Xh = generator.sample(n_samples=10000, n_trajs=ntrajs, x0=0.0, v0=0.0)
 X_val, idx_val, Xh_val = generator.sample(n_samples=10000, n_trajs=10, x0=0.0, v0=0.0)
 print("Real parameters", generator.get_coefficients())
 
-print("Initial ll", generator.score(X, idx_trajs=idx))
+initial_ll = generator.score(X, idx_trajs=idx)
+initial_ll_val = generator.score(X_val, idx_trajs=idx_val)
+print("Initial ll", initial_ll, initial_ll_val)
 
 basis = GLE_BasisTransform(basis_type="linear")
 # Trajectory estimation
-estimator = GLE_Estimator(init_params="random", dim_x=dim_x, dim_h=dim_h, basis=basis, model=model, EnforceFDT=False, OptimizeDiffusion=True, no_stop=True, max_iter=max_iter, n_init=1, random_state=random_state + 1, verbose=0, multiprocessing=8)
+estimator = GLE_Estimator(init_params="random", dim_x=dim_x, dim_h=dim_h, basis=basis, model=model, EnforceFDT=False, OptimizeDiffusion=True, no_stop=True, max_iter=max_iter, n_init=1, random_state=None, verbose=0, multiprocessing=8)
 # We set some initial conditions, check for stability
 # estimator.set_init_coeffs(generator.get_coefficients())
 
@@ -57,7 +57,9 @@ for i in range(N_big_steps):
 print(estimator.get_coefficients())
 
 
-plt.plot(logL_train, label="Log L train")
-plt.plot(logL_val, label="Log L validation")
+plt.plot(logL_train[1:], label="Log L train")
+plt.plot(logL_val[1:], label="Log L validation")
+plt.plot([initial_ll] * N_big_steps * max_iter, label="Initial ll train")
+plt.plot([initial_ll_val] * N_big_steps * max_iter, label="Initial ll validation")
 plt.legend(loc="upper right")
 plt.show()
